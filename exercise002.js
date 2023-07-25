67112cecbfea79d8ddb482a93329f54ab3e544b3
M Zia <murtaza_zia@hotmail.co.uk>
2023-07-25 19:52:54 +0100

    completed exercise002 task 1 and checked it in the terminal

--- exercise002.js
+++ exercise002.js
@@ -1,12 +1,29 @@
 import fetch from "node-fetch";
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+//const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
-};
+// const fetchData = (apiEndPoint) => {
+//   fetch(apiEndPoint)
+//     .then((response) => response.json())
+//     .then((json) => console.log(json))
+//     .catch((error) => console.log(error));
+// };
 
-fetchData(jsonTypicode);
+// fetchData(jsonTypicode);
+
+
+//Task 1; using async, await, try, catch
+const fetchDataFunction = async() => {
+  const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+
+  try {
+    const response = await fetch(jsonTypicode);
+    const json = await response.json();
+    console.log(json);
+  }
+  catch(error) {
+    console.log(error);
+  }
+}
+
+fetchDataFunction()
